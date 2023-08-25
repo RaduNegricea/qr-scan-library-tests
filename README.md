f8f63b272bb40dde8765566510a3d18c421469b3
RaduNegricea <126361435+RaduNegricea@users.noreply.github.com>
2023-08-25 19:22:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# qr-scan-library-tests
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngx-scanner-qrcode-vi576u)
---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  ScannerQRCodeConfig,
+  ScannerQRCodeResult,
+  NgxScannerQrcodeService,
+  NgxScannerQrcodeComponent,
+  ScannerQRCodeSelectedFiles,
+} from 'ngx-scanner-qrcode';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements AfterViewInit {
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#front_and_back_camera
+  public config: ScannerQRCodeConfig = {
+    constraints: {
+      video: {
+        width: window.innerWidth,
+      },
+    },
+    // canvasStyles: [
+    //   { /* layer */
+    //     lineWidth: 1,
+    //     fillStyle: '#00950685',
+    //     strokeStyle: '#00950685',
+    //   },
+    //   { /* text */
+    //     font: '17px serif',
+    //     fillStyle: '#ff0000',
+    //     strokeStyle: '#ff0000',
+    //   }
+    // ],
+  };
+
+  @ViewChild('action') action!: NgxScannerQrcodeComponent;
+
+  constructor(private qrcode: NgxScannerQrcodeService) {}
+
+  ngAfterViewInit(): void {
+    this.action.isReady.subscribe((res: any) => {
+      this.handle(this.action, 'start');
+    });
+  }
+
+  public handle(action: any, fn: string): void {
+    const playDeviceFacingBack = (devices: any[]) => {
+      // front camera or back camera check here!
+      const device = devices.find((f) =>
+        /back|rear|environment/gi.test(f.label)
+      ); // Default Back Facing Camera
+      action.playDevice(device ? device.deviceId : devices[0].deviceId);
+    };
+
+    if (fn === 'start') {
+      action[fn](playDeviceFacingBack).subscribe(
+        (r: any) => console.log(fn, r),
+        alert
+      );
+    } else {
+      action[fn]().subscribe((r: any) => console.log(fn, r), alert);
+    }
+  }
+}
